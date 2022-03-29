5b6dc936b92c527c8b7fe2fef612d6baa536d518
alexxpercy <emily.percy@students.plymouth.ac.uk>
2022-03-29 14:57:03 +0100

    v1.0

--- task1b.sv
+++ task1b.sv
@@ -14,7 +14,17 @@ A	B	C	Y
 
 module task1b(output logic [1:0] Y, input logic A, B, C);
 
+//use dataflow type, behavioural would involve reg instead of wires
+//create internal values for each Y bit
+logic Y0;
+logic Y1;
 
+//create logic statements
+assign Y0 = (~A & ~B & C) | (~A & B & ~C) | (A & ~B & ~C) | (A & B & C);
+assign Y1 = (~A & B & C) | (A & ~B & C) | (A & B & ~C) | (A & B & C);
+
+//assign to output
+Y[1:0] = {Y1, Y0};
 
 endmodule
 
