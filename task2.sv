74e61d97a546fb892d3d6a2ec90be7fcb39a45de
alexxpercy <emily.percy@students.plymouth.ac.uk>
2022-03-30 17:26:18 +0100

    TASK 2 - v3.0
    no errors

--- task2.sv
+++ task2.sv
@@ -16,26 +16,29 @@ n_RESET - if 0, do nothing
 logic xor_14;	//to go to bit 13
 logic xor_13;	//to go to bit 12
 logic xor_11;	//to go to bit 10
+//create feedback bit to be passed back
+logic feedback_bit;
+int i;
 
 //begin execution
 initial
 begin
-end
 
 //always activate if CLK goes high or reset changes
-always_ff @ (posedge CLK, negedge n_RESET)
+ @ (posedge CLK, negedge n_RESET)
 	//if reset, takes priority over all
 	if (~n_RESET)
 		//if reset is activated, do nothing
 		Q <= '0;
 	//otherwise, reset will be high, so if clock has hit edge
-		
-always_comb
-	Q <= N;
+	else
+	
+Q <= N;
 		//create FOR loop for all combinations - 2^n - 1 = 65535
-		generate for(genvar i = 0; i < 65535; i++) 
+		for(i = 0; i < 65535; i++) 
 		begin
 			//set feedback bit as first value
+			feedback_bit = Q & 1;
 			//find xor values
 			xor_14 <= (Q ^ (Q >> 14)) & 1;
 			xor_13 <= (Q ^ (Q >> 13)) & 1;
@@ -43,7 +46,7 @@ always_comb
 			//shift all to right and OR all xor bits to left, set feedback bit to start of array
 			Q <= (Q >> 1) | (feedback_bit << 15) | (xor_14 << 13) | (xor_13 << 12) | (xor_11 << 10);
 		end
-		end generate
+end
 
 
 endmodule
--- task2_tb.sv
+++ task2_tb.sv
@@ -35,10 +35,10 @@ for (i = 0; i < 131070; i++);
 		#10ps
 		//assert initial value at start - triggered at posedge, so when clk = 1
 		case(i)
-			0: if (q == N) $info("Initial run - PASS"); else $error("Initial run - FAIL");
-			2: if (q == 16'b1110001001110000) $info("Iteration 1 - PASS"); else $error("Iteration 1 - FAIL");
-			4: if (q == 16'b0111000100111000) $info("Iteration 2 - PASS"); else $error("Iteration 2 - FAIL");
-			6: if (q == 16'b0011100010011100) $info("Iteration 3 - PASS"); else $error("Iteration 3 - FAIL");
+			0: if (q == N) $write("Initial run - PASS"); else $error("Initial run - FAIL");
+			2: if (q == 16'b1110001001110000) $write("Iteration 1 - PASS"); else $error("Iteration 1 - FAIL");
+			4: if (q == 16'b0111000100111000) $write("Iteration 2 - PASS"); else $error("Iteration 2 - FAIL");
+			6: if (q == 16'b0011100010011100) $write("Iteration 3 - PASS"); else $error("Iteration 3 - FAIL");
 			default;
 		endcase
 	end
