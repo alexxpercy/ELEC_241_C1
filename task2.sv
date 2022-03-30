b92d50ed257e9782634875ecad446f6665dff732
alexxpercy <emily.percy@students.plymouth.ac.uk>
2022-03-30 02:20:44 +0100

    Beginning of initialisation

--- task2.sv
+++ task2.sv
@@ -3,5 +3,25 @@ module task2 #(parameter N=16'b1010110011100001) (output logic [15:0] Q, input l
 
 //Write solution here
 
+/*
+Explanation of all variables:
+N - original value to be entered into array
+Q - array to insert everything
+CLK - clock, edge triggered
+n_RESET - if 0, do nothing
+*/
+
+//begin execution
+initial
+//always activate if CLK goes high or reset changes
+always_ff @ (posedge CLK, negedge n_RESET)
+	//if reset, takes priority over all
+	if (~n_RESET)
+		//if reset is activated, do nothing
+		Q <= '0;
+	//otherwise, reset will be high, so if clock has hit edge
+	else
+		
+
 
 endmodule
