2586b4e9b75e20a8807b952e8422b4a55dafd059
alexxpercy <emily.percy@students.plymouth.ac.uk>
2022-03-30 14:58:48 +0100

    v1.1
    Overcoming errors

--- task2.sv
+++ task2.sv
@@ -12,7 +12,6 @@ n_RESET - if 0, do nothing
 */
 
 //name internal variables
-logic feedback_bit;	//bit to be XOR-ed
 //bits to be XOR-ed and put into new place
 logic xor_14;	//to go to bit 13
 logic xor_13;	//to go to bit 12
@@ -20,10 +19,8 @@ logic xor_11;	//to go to bit 10
 
 //begin execution
 initial
-
-//set starting parameter to array Q
-//array to be 10-1-01-10011100001
-Q <= N;
+begin
+end
 
 //always activate if CLK goes high or reset changes
 always_ff @ (posedge CLK, negedge n_RESET)
@@ -32,24 +29,21 @@ always_ff @ (posedge CLK, negedge n_RESET)
 		//if reset is activated, do nothing
 		Q <= '0;
 	//otherwise, reset will be high, so if clock has hit edge
-	else
-		//input feedback bit into position 0
-		Q <= {feedback_bit, Q[15:1]};
 		
 always_comb
-	begin
+	Q <= N;
 		//create FOR loop for all combinations - 2^n - 1 = 65535
-		for(int i = 0; i < 65535; i++) begin
+		generate for(genvar i = 0; i < 65535; i++) 
+		begin
 			//set feedback bit as first value
-			feedback_bit <= Q[0];
 			//find xor values
-			xor_14 <= (feedback_bit ^ Q[14]) & 1;
-			xor_13 <= (feedback_bit ^ Q[13]) & 1;
-			xor_11 <= (feedback_bit ^ Q[11]) & 1;
+			xor_14 <= (Q ^ (Q >> 14)) & 1;
+			xor_13 <= (Q ^ (Q >> 13)) & 1;
+			xor_11 <= (Q ^ (Q >> 11)) & 1;
 			//shift all to right and OR all xor bits to left, set feedback bit to start of array
 			Q <= (Q >> 1) | (feedback_bit << 15) | (xor_14 << 13) | (xor_13 << 12) | (xor_11 << 10);
 		end
-	end
+		end generate
 
 
 endmodule
